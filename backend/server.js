246fb384b4f42456d31a22f43a69b2d99c816dcb
eyobt4 <eyobtesfaye838@gmail.com>
2025-08-06 23:54:44 +0300

    working folders set

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,12 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+// server.ts
+import { express } from "express"
+import { ExpressAuth } from "@auth/express"
+import GitHub from "@auth/express/providers/github"
+app.use("/auth/*", ExpressAuth({ providers: [GitHub] }))
+
 const app = express();
 
 app.use(cors);
