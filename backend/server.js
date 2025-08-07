302517f7c98579f13ff740bb8b41c2fcbedfc5ce
eyobt4 <eyobtesfaye838@gmail.com>
2025-08-08 00:04:34 +0300

    signup api added

--- backend/server.js
+++ backend/server.js
@@ -3,41 +3,69 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const session = require("express-session")
-const auth = require("../auth")
+// const auth = require("../auth");
+const User = require("./models/User");
 const app = express();
 
-app.use(cors);
-app.use(express.json());
-app.use(
-    session({
-        secret: "supersecretkey",
-        resave: false,
-        saveUninitialized: true,
-        cookie: {secure:false},
-    })
-);
-app.use("/auth",auth);
+//session
+// app.use(cors);
+// app.use(express.json());
+// app.use(
+//     session({
+//         secret: "supersecretkey",
+//         resave: false,
+//         saveUninitialized: true,
+//         cookie: {secure:false},
+//     })
+// );
+// app.use("/auth",auth);
+// app.use(express.static("public"));
 app.get("/",(req,res)=>{
     res.send("API is working");
 });
-app.use(express.static("public"));
 const port = process.env.PORT;
 
-//signup
+//signup user
 app.post("/signup",(req,res)=>{
-            
+    //recive the req
+    const {username,email,password} = req.body;
+    //check if the fields are empty
+    if(!username||!email||!password){
+        res.status(400);
+        res.send("all fields manadatory");
+    };
+    //check if the email available
+    const isAvailable = User.findOne({email});
+    if (isAvailable){
+        res.status(400);
+        res.send("user already exist");
+    };
+    //hash password
+    const hashPassword = bcrypt.hash(password,10);
+    //create the user
+    const newUser = User.create({
+        username,
+        email,
+        hashPassword,
+    });
+    newUser.save();
+    res.status(201).send("user succesfuly created");
+    console.log("user succesfuly created");
 });
-//login
-app.post("/signup",(req,res)=>{
-            
+
+//login user
+app.post("/login",(req,res)=>{
+    
 });
+//update user
+//delete user
 //connecting the database
 mongoose.connect(process.env.MONGO_URL,{
     useNewUrlParser : true,
     useUnifiedTopology: true,
 })
-.then(()=>console.log("mongoDB connected"))
-.catch(()=>console.log("mongoDB connection error",err));
+.then(()=>console.log("mongoDB connected"+ connect.connection.name))
+.catch(()=>console.log("mongoDB connection error"));
 
 // protected /dashbord route
 app.get("/dashbard",(req,res)=>{
@@ -46,11 +74,11 @@ app.get("/dashbard",(req,res)=>{
 }
     else{
         res.status(401);
-        res.send("Unauthorized");
+        res.send("Unauthorized route");
     }
 });
 
 app.listen(port,(req,res)=>{
-    console.log( `server running at ${port}`);
+    console.log( "server running at" +port);
     
-})
+});
