210e7659b3f9fbf53da63288d10c66bdb25085ce
eyobt4 <eyobtesfaye838@gmail.com>
2025-08-14 22:52:09 +0300

    auth second section added

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const connectDB = require("./config/connectDB");
+// const connectDB = require("./config/connectDB");
 const app = express();
 const cookieParser =require( "cookie-parser");
 const userRouter = require("./routes/userRouter");
@@ -11,21 +11,113 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-connectDB();
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const connectionDb = () => {
+    try{
+    const connect = mongoose.connect(process.env.MONGO_URL);
+    console.log("database connected");
+    
+}
+catch(error){
+    console.log(error);
+    
+}
+};
+
+connectionDb();
+
+const newSchema = mongoose.Schema({
+    username:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+},
+{
+timestamp:true
+}
+);
+
 
 app.get("/",(req,res)=>{
     res.send("API is working");
 });
-// app.use("/api/auth",userRouter);
 
 //protected route /dashboard
-app.post("/signup",(req,res)=>{
-    res.status(200);
-    res.json({message:"signup sucessful"})  
-    console.log("signup sucessful",res.json);
-         
+app.post("/signup",async (req,res)=>{
+    const {username,email,password} = req.body;
+    if(!username||!password){
+        res.status(404).send("all fields required");
+    };
+    const available = await newSchema.findOne({email});
+    if(available){
+        res.status(404).send("user already exist");
+    };
+
+    const hashPassword = bcrypt.hash(password,10);
+    const newUser = newSchema.create({
+        username,
+        email,
+        hashPassword
+    })
+    newUser.save();
+    console.log(newUser);
+    
+    try{
+        const newUser = new User({username,email,password})
+        await newUser.save();
+        console.log("newUser is:",newUser);
+        
+    }
+    catch(error){
+
+    }
     
 });
+
+app.post("/login",async (req,res)=>{
+    const{email,password}= req.body;
+
+    if(!username||!password){
+        res.status(404).send("all fields required");
+    };
+    const available = await newSchema.findOne({email});
+    if(available){
+        const userLogin = await bcrypt.compare(available.password = password);
+
+    }
+    const accessToken = jwt.sign({},{JWT_SECRET},{expiresIn:2m});
+    const refreshToken = jwt.sign({},{JWT_SECRET},{expiresIn:7d});
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //server listen
