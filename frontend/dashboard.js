ccf5117fb742c299740578db12693a6660950de9
You <you@example.com>
2025-08-26 16:40:36 +0300

    create post res

--- frontend/dashboard.js
+++ frontend/dashboard.js
@@ -4,31 +4,37 @@ const checkAuth = async ()=>{
         windows.locaion.href = "index.html";
         return;
     }
-    const createblog= document.getElementById("createblog");
+}
 
+    const createblog= document.getElementById("createblog");
+    console.log(createblog.value);
     createblog.addEventListener("submit",async (e)=>{
         e.preventDefault();
 
         const form = new FormData(createblog);
         const data = Object.fromEntries(form.entries());
+        console.log(data);
+        
+        // const form = new FormData(createblog);
+        // console.log(form);
+        // const data = Object.fromEntries(form.entries());
+        
         try{
 
-            response = fetch("http://localhost:5001/createblog",{
+            const response = fetch("http://localhost:5001/createblog",{
                 method:"POST",
                 headers:{"content-Type":"application/json"},
-                body:JSON.stringify(data)
+                body:JSON.stringify(data),
+                
             });
-
+            console.log(response);
             if(response.ok){
                 windows.locaion.href = "Home.html"
             }
             else{
-                console.log("post not send to backend");
-                res.status(400).json({message:"post not send to backend"})
-            }
+                console.log("post not send to backend");            }
         }
         catch(error){
             console.log(error);
         }
     });
-}
